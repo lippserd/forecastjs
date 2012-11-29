0eb416c7b1f0f0b453e93248ef87a64853d2755e
Eric Lippmann <eric.lippmann@netways.de>
2012-11-29 14:49:07 +0100

    Add library initialization

--- forecast.js
+++ forecast.js
@@ -3,3 +3,22 @@
  * Copyright (C) 2012 Eric Lippmann
  * forecast.js may be freely distributed under the MIT license.
  */
+
+/**
+ * Time series forecasting models.
+ */
+; (function () {
+    'use strict';
+
+    var // Reference to `window` in the browser, `exports`, `global` on
+        // the server
+        root = this;
+
+    function forecast () {
+        if (!(this instanceof forecast)) {
+            return new forecast();
+        }
+    }
+
+    root.forecast = forecast();
+}.call(this));
